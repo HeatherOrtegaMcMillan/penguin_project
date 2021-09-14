6c2fd52f258ba48abdc6dbba462a89598cba7fc7
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-09-14 13:27:29 -0500

    add drop cols arguement to prep function

--- prepare.py
+++ prepare.py
@@ -4,9 +4,11 @@ import pandas as pd
 import numpy as np 
 
 
-def prep_penguins(df):
+# clean and prep function
+def prep_penguins(df, dropcols = True):
     # drop island and sex columns
-    penguins_clean = df.drop(columns=['island', 'sex'])
+    if dropcols == True: 
+        df = df.drop(columns=['island', 'sex'])
     
     # create dictionary of old column names, new names removed the measurements
     col_names = {'bill_length_mm': 'bill_length', 
@@ -15,14 +17,14 @@ def prep_penguins(df):
                  'body_mass_g': 'body_mass'}
 
     # rename columns
-    penguins_clean = penguins_clean.rename(columns=col_names)
+    df = df.rename(columns=col_names)
     
     # drop the 2 rows that had null values (3 and 339)
-    penguins_clean = penguins_clean.dropna(axis=0)
+    df = df.dropna(axis=0)
     
     # split data using sklearn train test split
     # 20% test 80% train_validate (70% train, 30% validate)
-    train, test = train_test_split(penguins_clean, test_size=0.2, random_state=713, stratify=penguins_clean.species)
+    train, test = train_test_split(df, test_size=0.2, random_state=713, stratify=df.species)
     train, validate = train_test_split(train, train_size=0.7, random_state=713, stratify=train.species)
     
     return train, validate, test
